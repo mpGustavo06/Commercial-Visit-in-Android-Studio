e5c0877e2ca9205244a567b04c13293c31bb5a0c
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2025-02-19 20:05:31 -0300

    feat: implemented client creation, city verification, and address lookup

--- app/src/main/java/com/example/visitacomercial/CadastroClienteActivity.java
+++ app/src/main/java/com/example/visitacomercial/CadastroClienteActivity.java
@@ -12,12 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.room.Room;
 
 import com.example.visitacomercial.Control.CidadeDAO;
 import com.example.visitacomercial.Control.ClienteDAO;
 import com.example.visitacomercial.Control.ServiceViaCep;
-import com.example.visitacomercial.Database.AppDatabase;
 import com.example.visitacomercial.Models.Cidade;
 import com.example.visitacomercial.Models.Cliente;
 import com.example.visitacomercial.Models.Endereco;
@@ -26,7 +24,7 @@ import java.io.IOException;
 
 public class CadastroClienteActivity extends AppCompatActivity
 {
-    private EditText editTextCep, editTextCnpj, editTextRazaoSocial, editTextNomeFantasia, editTextTelefone, editTextEmail, editTextLogradouro, editTextBairro, editTextNumero, editTextCidade, editTextIbge;
+    private EditText editTextCep, editTextCnpj, editTextRazaoSocial, editTextNomeFantasia, editTextTelefone, editTextEmail, editTextLogradouro, editTextBairro, editTextNumero, editTextCidade, editTextIbge, editTextContatoPessoa;
     private Button buttonBuscarEndereco, buttonSalvarCliente;
     private String zipcode, end;
     private Endereco endereco;
@@ -50,13 +48,14 @@ public class CadastroClienteActivity extends AppCompatActivity
 
         // Inicialização do DAO
         cidadeDAO = new CidadeDAO(getApplicationContext(), getLifecycle());
-        clienteDAO = new ClienteDAO(getApplicationContext());
+        clienteDAO = new ClienteDAO(getApplicationContext(), getLifecycle());
 
         // Inicialização dos componentes da interface
         editTextCep = findViewById(R.id.editTextCep);
         editTextCnpj = findViewById(R.id.editTextCnpj);
         editTextRazaoSocial = findViewById(R.id.editTextRazaoSocial);
         editTextNomeFantasia = findViewById(R.id.editTextNomeFantasia);
+        editTextContatoPessoa = findViewById(R.id.editTextContatoPessoa);
         editTextTelefone = findViewById(R.id.editTextTelefone);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextLogradouro = findViewById(R.id.editTextLogradouro);
@@ -89,94 +88,271 @@ public class CadastroClienteActivity extends AppCompatActivity
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    public void buscarEndereco(View view)
-    {
-        getCepFromEditText();
-
-        Thread thread = new Thread(() -> {
-            try
-            {
-                endereco = ServiceViaCep.buscarEnderecoViaCep(zipcode);
-                Log.d("BUSCAR.END.IN.THREAD", "ENDERECO IN: " + endereco);
-
-                if (endereco.isErro())
-                {
-                    runOnUiThread(() -> {
-                        Toast.makeText(this, "CEP inválido!", Toast.LENGTH_SHORT).show();
-                    });
-                    return;
-                }
-            }
-            catch (IOException e)
-            {
-                runOnUiThread(() -> {
-                    Toast.makeText(this, "Erro ao buscar endereço por CEP", Toast.LENGTH_SHORT).show();
-                });
-            }
-        });
-
-        thread.start();
-
-        try
-        {
-            thread.join();
-            Log.d("BUSCAR.END.OUT.THREAD", "ENDERECO OUT: "+endereco);
-
-            verificarCidadeExiste();
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-
-        Log.d("BUSCAR.ENDERECO", "CEP: "+zipcode);
-    }
-
-    //--------------------------------------------------------------------------------------------//
     public void salvarCliente(View view)
     {
         Cliente cliente = new Cliente();
-        cliente.setCnpj(editTextCnpj.getText().toString());
-        cliente.setSocialReason(editTextRazaoSocial.getText().toString());
-        cliente.setFantasyName(editTextNomeFantasia.getText().toString());
-        cliente.setPhone(editTextTelefone.getText().toString());
-        cliente.setEmail(editTextEmail.getText().toString());
-        cliente.setZipCode(zipcode);
-        cliente.setStreet(editTextLogradouro.getText().toString());
-        cliente.setNumber(editTextNumero.getText().toString());
-        cliente.setDistrict(editTextBairro.getText().toString());
-        cliente.setIbgeCity(Long.valueOf(editTextIbge.getText().toString()));
+        String cnpj;
+
+        if ( isNotNull(editTextCnpj.getText().toString()) )
+        {
+            cnpj = editTextCnpj.getText().toString();;
+        }
+        else
+        {
+            Toast.makeText(this, "CNPJ está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextRazaoSocial.getText().toString()) )
+        {
+            cliente.setSocialReason(editTextRazaoSocial.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Razão Social está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextNomeFantasia.getText().toString()) )
+        {
+            cliente.setFantasyName(editTextNomeFantasia.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Nome Fantasia está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextTelefone.getText().toString()) )
+        {
+            cliente.setPhone(editTextTelefone.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Telefone está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextContatoPessoa.getText().toString()) )
+        {
+            cliente.setPhone(editTextContatoPessoa.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Responsável está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextEmail.getText().toString()) )
+        {
+            cliente.setEmail(editTextEmail.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Email está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextLogradouro.getText().toString()) )
+        {
+            cliente.setStreet(editTextLogradouro.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Logradouro está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextBairro.getText().toString()) )
+        {
+            cliente.setDistrict(editTextBairro.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Bairro está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextNumero.getText().toString()) )
+        {
+            cliente.setNumber(editTextNumero.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "Número está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextCidade.getText().toString()) )
+        {
+            cliente.setIbgeCity(Long.parseLong(editTextIbge.getText().toString()));
+        }
+        else
+        {
+            Toast.makeText(this, "IBGE está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ( isNotNull(editTextCep.getText().toString()) )
+        {
+            cliente.setZipCode(editTextCep.getText().toString());
+        }
+        else
+        {
+            Toast.makeText(this, "CEP está Nulo!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if (cnpj.length() >= 14 && cnpj.length() <= 18)
+        {
+            if (cnpj.contains("."))
+                cnpj = cnpj.replace(".", "");
+
+            if (cnpj.contains("-"))
+                cnpj = cnpj.replace("-", "");
+
+            if (cnpj.contains("/"))
+                cnpj = cnpj.replace("/", "");
+
+            try
+            {
+                Double cnpjNumber = Double.parseDouble(cnpj);
+                cliente.setCnpj(cnpj);
+            }
+            catch (NumberFormatException e)
+            {
+                Toast.makeText(this, "CNPJ inválido!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "CNPJ é maior ou menor que o válido!", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         Log.d("SALVAR.CLI", "CLIENTE: "+cliente);
-        clienteDAO.inserirCliente(cliente);
+        boolean sucess = clienteDAO.inserirCliente(cliente);
+
+        if (sucess)
+        {
+            Toast.makeText(this, "Cliente inserido com sucesso!", Toast.LENGTH_SHORT).show();
+            apagarCampos();
+        }
+        else
+        {
+            Toast.makeText(this, "Erro ao inserir cliente!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    public void buscarEndereco(View view)
+    {
+        boolean sucess = getCepFromEditText();
+
+        if (sucess == true)
+        {
+            Thread thread = new Thread(() -> {
+                try
+                {
+                    endereco = ServiceViaCep.buscarEnderecoViaCep(zipcode);
+                    Log.d("BUSCAR.END.IN.THREAD", "ENDERECO IN: " + endereco);
+
+                    if (endereco.isErro())
+                    {
+                        runOnUiThread(() -> {
+                            Toast.makeText(this, "CEP inválido!", Toast.LENGTH_SHORT).show();
+                        });
+                        return;
+                    }
+                }
+                catch (IOException e)
+                {
+                    runOnUiThread(() -> {
+                        Toast.makeText(this, "Erro ao buscar endereço por CEP", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            });
+
+            thread.start();
+
+            try
+            {
+                thread.join();
+                Log.d("BUSCAR.END.OUT.THREAD", "ENDERECO OUT: " + endereco);
+
+                verificarCidadeExiste();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+            Log.d("BUSCAR.ENDERECO", "CEP: " + zipcode);
+        }
+        else
+        {
+            Toast.makeText(this, "CEP inválido!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    public void getCepFromEditText()
+    public boolean getCepFromEditText()
     {
-        if (!editTextCep.getText().toString().contains("-"))
+        if (editTextCep.getText().toString().length() >= 8 && editTextCep.getText().toString().length() <= 9)
         {
-            zipcode = editTextCep.getText().toString();
+            if (!editTextCep.getText().toString().contains("-"))
+            {
+                zipcode = editTextCep.getText().toString();
+            }
+
+            if (editTextCep.getText().toString().contains("-"))
+            {
+                zipcode = editTextCep.getText().toString().replace("-", "");
+            }
+
+            return true;
         }
 
-        if ( editTextCep.getText().toString().contains("-"))
-        {
-            zipcode = editTextCep.getText().toString().replace("-" , "");
-        }
+        zipcode = "";
+        Toast.makeText(this, "CEP inválido!", Toast.LENGTH_SHORT).show();
+        return false;
     }
 
     //--------------------------------------------------------------------------------------------//
     public void verificarCidadeExiste()
     {
         Log.d("VERIFICAR.CTY.EXISTE", "INICIO");
-        Log.d("VERIFICAR.CTY.EXISTE", "IBGE: "+endereco.getIbge());
 
-        Long l = Long.parseLong(endereco.getIbge());
+
+        Long l = 0L;
+
+        try
+        {
+            if (endereco.getIbge() != null)
+            {
+                l = Long.parseLong(endereco.getIbge());
+                Log.d("VERIFICAR.CTY.EXISTE", "IBGE: "+endereco.getIbge());
+            }
+            else
+            {
+                Log.d("VERIFICAR.CTY.EXISTE", "endereco.getIbge(): null");
+                Toast.makeText(this, "Cidade inválida!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            Log.d("VERIFICAR.CTY.EXISTE", "NumberFormatException: "+e.getMessage());
+            return;
+        }
+
         Log.d("VERIFICAR.CTY.EXISTE.LONG", "IBGE LONG: "+l);
 
         cidadeExiste = cidadeDAO.verificarCidadeExiste(l);
 
-        if (cidadeExiste == false)
+        if (cidadeExiste)
         {
             Cidade cidade = new Cidade();
             cidade.setIbge(Long.parseLong(endereco.getIbge()));
@@ -195,6 +371,7 @@ public class CadastroClienteActivity extends AppCompatActivity
         adicionarEnderecoNaTela();
     }
 
+    //--------------------------------------------------------------------------------------------//
     public void adicionarEnderecoNaTela()
     {
         editTextLogradouro.setText(endereco.getLogradouro());
@@ -203,5 +380,28 @@ public class CadastroClienteActivity extends AppCompatActivity
         editTextIbge.setText(endereco.getIbge());
 
         Log.d("ENDERECO.NA.TELA", "IBGE: "+editTextIbge.getText().toString());
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    public void apagarCampos()
+    {
+        editTextCep.setText("");
+        editTextCnpj.setText("");
+        editTextRazaoSocial.setText("");
+        editTextNomeFantasia.setText("");
+        editTextTelefone.setText("");
+        editTextEmail.setText("");
+        editTextLogradouro.setText("");
+        editTextBairro.setText("");
+        editTextNumero.setText("");
+        editTextCidade.setText("");
+        editTextIbge.setText("");
+        editTextContatoPessoa.setText("");
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    public static boolean isNotNull(String texto)
+    {
+        return texto != null && !texto.trim().isEmpty();
     }
 }
--- app/src/main/java/com/example/visitacomercial/MainActivity.java
+++ app/src/main/java/com/example/visitacomercial/MainActivity.java
@@ -15,15 +15,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
 import com.example.visitacomercial.Adapter.ClientesAdapter;
 import com.example.visitacomercial.Adapter.SpinnerCityAdapter;
 import com.example.visitacomercial.Control.CidadeAPIService;
+import com.example.visitacomercial.Control.CidadeDAO;
 import com.example.visitacomercial.Control.ClienteAPIService;
-import com.example.visitacomercial.Control.VisitaAPIService;
+import com.example.visitacomercial.Control.ClienteDAO;
 import com.example.visitacomercial.Database.AppDatabase;
 import com.example.visitacomercial.Models.Cidade;
 import com.example.visitacomercial.Models.Cliente;
@@ -31,6 +31,7 @@ import com.example.visitacomercial.Utils.NetworkUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -38,24 +39,43 @@ public class MainActivity extends AppCompatActivity
     private Button buttonCadastrarCliente;
     private ListView listViewClientes;
 
-    private Cliente cliente;
     private Cliente clienteSelected = new Cliente();
+    private ClienteDAO clienteDAO;
     private List<Cliente> clientesList = new ArrayList<>();
-    private ClienteAPIService clienteAPIService;
     private ClientesAdapter clientesAdapter;
-
-    private VisitaAPIService visitaAPIService;
+    private ClienteAPIService clienteApiService;
 
     private Cidade cidadeSelected = new Cidade();
-    private CidadeAPIService cidadeAPIService;
-
+    private CidadeDAO cidadeDAO;
+    private CidadeAPIService cidadeApiService;
     private SpinnerCityAdapter spinnerCityAdapter;
 
     private AppDatabase database;
 
+    // Observers
+    Observer<List<Cidade>> observerCidades = new Observer<List<Cidade>>() {
+        @Override
+        public void onChanged(List<Cidade> cidades)
+        {
+            spinnerCityAdapter.clear();
+            spinnerCityAdapter.addAll(cidades);
+            spinnerCityAdapter.notifyDataSetChanged();
+        }
+    };
+
+    Observer<List<Cliente>> observerClientes = new Observer<List<Cliente>>() {
+        @Override
+        public void onChanged(List<Cliente> clientes)
+        {
+            clientesAdapter.clear();
+            clientesAdapter.addAll(clientes);
+            clientesAdapter.notifyDataSetChanged();
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -66,11 +86,12 @@ public class MainActivity extends AppCompatActivity
             return insets;
         });
 
-        Log.d("CIDADE", "Chamada a função inserirCidadeManual()");
+        // Inicialização do DAO
+        cidadeDAO = new CidadeDAO(getApplicationContext(), getLifecycle());
+        clienteDAO = new ClienteDAO(getApplicationContext(), getLifecycle());
 
         //Testando se existe conexão com a internet
-        if (!NetworkUtils.isConnected(this))
-        {
+        if (!NetworkUtils.isConnected(this)) {
             Toast.makeText(this, "Sem conexão com a internet. O aplicativo não pode ser usado offline.", Toast.LENGTH_LONG).show();
             finish();
         }
@@ -78,56 +99,67 @@ public class MainActivity extends AppCompatActivity
         // Inicialização dos componentes da interface
         buttonCadastrarCliente = findViewById(R.id.buttonCadastrarCliente);
         listViewClientes = findViewById(R.id.ListViewClientes);
+        spinnerCidades = findViewById(R.id.spinnerCities);
 
         //Adapter & List
-        clientesAdapter = new ClientesAdapter(this, (ArrayList<Cliente>) clientesList);
+        if (clienteDAO.getClientes() != null)
+        {
+            clientesAdapter = new ClientesAdapter(this, clienteDAO.getClientes());
+        }
+        else
+        {
+            clientesAdapter = new ClientesAdapter(this, new ArrayList<>());
+        }
         listViewClientes.setAdapter(clientesAdapter);
         listViewClientes.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         // Configuaração do Spinner
-        /**spinnerCityAdapter = new SpinnerCityAdapter(this, new ArrayList<>());
-        spinnerCidades.setAdapter(spinnerCityAdapter);*/
-
-        // Observe o LiveData
-//        LiveData<List<Cidade>> citiesLiveData = cidadeAPIService.getCities();
-//        citiesLiveData.observe(this, new Observer<List<Cidade>>() {
-//            @Override
-//            public void onChanged(List<Cidade> cities)
-//            {
-//                // Atualize o adapter do Spinner com a nova lista de cidades
-//                spinnerCityAdapter.clear();
-//                spinnerCityAdapter.addAll(cities);
-//                spinnerCityAdapter.notifyDataSetChanged();
-//            }
-//        });
+        if (cidadeDAO.getCidades() != null)
+        {
+            spinnerCityAdapter = new SpinnerCityAdapter(this, cidadeDAO.getCidades());
+        }
+        else
+        {
+            spinnerCityAdapter = new SpinnerCityAdapter(this, new ArrayList<>());
+        }
+        spinnerCidades.setAdapter(spinnerCityAdapter);
 
         // Eventos
-//        spinnerCidades.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-//        {
-//            boolean isFirstSelection = true;
-//
-//            @Override
-//            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
-//            {
-//                if (isFirstSelection)
-//                {
-//                    isFirstSelection = false;
-//                    return;
-//                }
-//
-//                cidadeSelected = (Cidade) adapterView.getItemAtPosition(position);
-//                Log.d("SPINNER.CTY.TESTE", "MODELO: "+ cidade.toString());
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent)
-//            {
-//                Log.d("SPINNER.CTY.ERROR", "Nenhuma cidade selecionado.");
-//            }
-//        });
-
-        listViewClientes.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        spinnerCidades.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
         {
+            boolean isFirstSelection = true;
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                if (isFirstSelection)
+                {
+                    isFirstSelection = false;
+                    return;
+                }
+
+                cidadeSelected = (Cidade) adapterView.getItemAtPosition(position);
+                Log.d("SPINNER.CIDADE.SELECTED", "CIDADE SELECT: " + cidadeSelected);
+
+                clienteApiService.getClientPerCity(cidadeSelected.getIbge()).observe(MainActivity.this, new Observer<List<Cliente>>() {
+                    @Override
+                    public void onChanged(List<Cliente> clientes)
+                    {
+                        clientesAdapter.clear();
+                        clientesAdapter.addAll(clientes);
+                        clientesAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                Log.d("SPINNER.CTY.ERROR", "Nenhuma cidade selecionado.");
+            }
+        });
+
+        listViewClientes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
             {
@@ -136,6 +168,35 @@ public class MainActivity extends AppCompatActivity
                 clienteSelected = (Cliente) adapterView.getItemAtPosition(position);
             }
         });
+
+        buttonCadastrarCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                abrirCadastroCliente(view);
+            }
+        });
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    public void onStart()
+    {
+        super.onStart();
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "app_database")
+                .setQueryCallback((sqlQuery, bindArgs) -> { Log.d("ROOM.QUERY.||||||||||", "SQL: " + sqlQuery); }, Executors.newSingleThreadExecutor())
+                .build();
+
+        cidadeApiService = database.cidadeService();
+        cidadeApiService.getCities().observe(this, observerCidades);
+
+        clienteApiService = database.clienteService();
+        clienteApiService.getClients().observe(this, observerClientes);
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    public void onStop()
+    {
+        super.onStop();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -145,21 +206,11 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    public void onStart()
+    public void abrirCadastroVisita(View view)
     {
-        super.onStart();
-        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "app_database").build();
-
-        clienteAPIService = database.clienteDAO();
-        visitaAPIService = database.visitaDAO();
-        cidadeAPIService = database.cidadeDAO();
-    }
-
-    public void onStop()
-    {
-        database.close();
-        super.onStop();
+        Intent intent = new Intent(this, VisitaActivity.class);
+        intent.putExtra("cliente", clienteSelected);
+        startActivity(intent);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
